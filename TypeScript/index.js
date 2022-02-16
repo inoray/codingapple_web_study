74616303b5c8bff73723a093f644b7a266e60077
sunghunkim <sunghunk@gmail.com>
2022-02-16 20:20:45 +0900

    [TypeScript] 타입스크립트로 HTML 변경과 조작할 때 주의점

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -8,3 +8,32 @@ var name_3 = "lee";
 function f(x) {
     return x * 2;
 }
+var func = function (a) {
+    return 10;
+};
+var info = {
+    name: "kim",
+    plusOne: function (a) {
+        return a + 1;
+    },
+    changeName: function () {
+        console.log("hi");
+    }
+};
+var image = document.getElementById("image");
+if (image instanceof HTMLImageElement) {
+    image.src = "test.jpg";
+}
+var link = document.getElementsByClassName("naver");
+for (var i = 0; i < link.length; ++i) {
+    var a = link[i];
+    if (a instanceof HTMLAnchorElement) {
+        a.href = "https://kakao.com";
+    }
+}
+var link2 = document.querySelectorAll(".naver");
+link2.forEach(function (a) {
+    if (a instanceof HTMLAnchorElement) {
+        a.href = "https://kakao.com";
+    }
+});
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -12,3 +12,46 @@ let name_3: MyType = "lee";
 function f(x: number): number {
   return x * 2;
 }
+
+type FuncType = (a : string) => number;
+
+let func : FuncType = function(a){
+  return 10;
+}
+
+// 객체 내부 함수 타입 지정
+type InfoType = {
+  name: string,
+  plusOne: (a: number) => number,
+  changeName: () => void
+}
+
+let info: InfoType = {
+  name: "kim",
+  plusOne(a){
+    return a + 1;
+  },
+  changeName: () => {
+    console.log("hi");
+  }
+}
+
+let image = document.getElementById("image");
+if (image instanceof HTMLImageElement) {
+  image.src = "test.jpg";
+}
+
+let link = document.getElementsByClassName("naver");
+for (let i = 0; i < link.length; ++i){
+  let a = link[i];
+  if (a instanceof HTMLAnchorElement){
+    a.href = "https://kakao.com";
+  }
+}
+
+let link2 = document.querySelectorAll(".naver");
+link2.forEach((a) =>{
+  if (a instanceof HTMLAnchorElement){
+    a.href = "https://kakao.com";
+  }
+})
