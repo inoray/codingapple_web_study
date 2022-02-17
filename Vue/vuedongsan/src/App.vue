0162443ca71d3995b98a6f54002ba0d0b756ae47
shkim <shkim@inzisoft.ocm>
2022-02-17 14:59:07 +0900

    [Vue] 모달창

--- Vue/vuedongsan/src/App.vue
+++ Vue/vuedongsan/src/App.vue
@@ -1,29 +1,49 @@
 <template>
+  <div class="black-bg" v-if="modalState" @click="modalState = false">
+    <div class="white-bg">
+      <img class="room-img" :src="onerooms[selected].image" />
+      <h4>{{ onerooms[selected].title }}</h4>
+      <p>{{ onerooms[selected].price }} 만원</p>
+      <p>{{ onerooms[selected].content }}</p>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="a in menu" :key="a"> {{ a }} </a>
   </div>
 
-  <div v-for="(a, i) in products" :key="i">
-    <img class="room-img" src="./assets/room0.jpg" />
-    <h4>{{ a.name }}</h4>
+  <div v-for="(a, i) in onerooms" :key="i">
+    <img class="room-img" :src="a.image" />
+    <h4
+      @click="
+        modalState = true;
+        selected = i;
+      "
+    >
+      {{ a.title }}
+    </h4>
     <p>{{ a.price }} 만원</p>
-    <button @click="increase">신고하기</button>
-    <span>신고수: {{ a.report }}</span>
+    <!-- <button @click="increase">신고하기</button>
+    <span>신고수: {{ a.report }}</span> -->
   </div>
 </template>
 
 <script>
+import data from "./assets/oneroom.js";
+
 export default {
   name: "App",
   data() {
     return {
+      modalState: false,
+      selected: 0,
       menu: ["Home", "Products", "About"],
       products: [
-        { name: "역삼동원룸", price: 50, report: 0 },
-        { name: "천호동원룸", price: 60, report: 0 },
-        { name: "마포구원룸", price: 70, report: 0 },
+        { title: "역삼동원룸", price: 50, report: 0 },
+        { title: "천호동원룸", price: 60, report: 0 },
+        { title: "마포구원룸", price: 70, report: 0 },
       ],
-      // products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      onerooms: data,
     };
   },
   methods: {
@@ -58,5 +78,25 @@ export default {
 .room-img {
   width: 100%;
   margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
