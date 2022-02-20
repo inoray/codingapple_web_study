b12f05f2a18d9820689fe3c95ec03c740cc31124
sunghunkim <sunghunk@gmail.com>
2022-02-20 16:57:17 +0900

    [Vue] blog 데이터 바인딩

---
+++ Vue/blog/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h5>{{a.title}}</h5>
+    <p>{{a.date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListList',
+  props: {
+    a: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
