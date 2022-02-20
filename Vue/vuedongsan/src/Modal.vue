35f5038fc7394c54f1e696fb6be3f368a5e8ec04
sunghunkim <sunghunk@gmail.com>
2022-02-20 10:13:54 +0900

    [Vue] input 기능 추가

--- Vue/vuedongsan/src/Modal.vue
+++ Vue/vuedongsan/src/Modal.vue
@@ -3,9 +3,13 @@
     <div class="white-bg">
       <img class="room-img" :src="onerooms[selected].image" />
       <h4>{{ onerooms[selected].title }}</h4>
-      <p>{{ onerooms[selected].price }} 원</p>
+      <p>{{ onerooms[selected].price * month }} 원</p>
       <p>{{ onerooms[selected].content }}</p>
       <button @click="$emit('closeModal')">닫기</button>
+      <p>입력값: {{month}}</p>
+      <!-- <input @input="month = $event.target.value"> -->
+      <input v-model.number="month">
+      <!-- 위에 있는 @input과 동일한 기능을 함 -->
     </div>
   </div>
 </template>
@@ -17,6 +21,19 @@ export default {
     onerooms: Object,
     selected: Number,
     modalState: Boolean,
+  },
+  data() {
+    return {
+      month: 0
+    }
+  },
+  watch: {
+    month(a) {
+      if (isNaN(a)) {
+        alert("문자입력 금지");
+        this.month = 1;
+      }
+    }
   },
 };
 </script>
